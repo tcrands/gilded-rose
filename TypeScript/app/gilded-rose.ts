0967594c38f1e4b2247de4da4110a015567d304a
Tom <tcrands@gmail.com>
2019-02-20 10:27:50 +0000

    Basic condition refactor so each block contains logic for each item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -18,76 +18,45 @@ export class GildedRose {
     }
 
     updateQuality() {
-        //Loop over all items
         for (let i = 0; i < this.items.length; i++) {
-            // Item is not aged brie or BSP
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                // Item has a quantity
-                if (this.items[i].quality > 0) {
-                    // Item is not Sulfara
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        // Decrease quantity
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                // Items quality if more than 50 and if either aged brie or BSP
+            const itemName = this.items[i].name
+            if (itemName === 'Aged Brie') {
                 if (this.items[i].quality < 50) {
-                    // Increase quality
-                    this.items[i].quality = this.items[i].quality + 1
-                    // Item id BSP
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        // sellIn more than 11
-                        if (this.items[i].sellIn < 11) {
-                            //quality more than 50
-                            if (this.items[i].quality < 50) {
-                                // Increase quality
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        // Sellin less than 11 but more than 6
-                        if (this.items[i].sellIn < 6) {
-                            //Quality more than 50
-                            if (this.items[i].quality < 50) {
-                                // Increase quality
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
+                    this.items[i].quality++
                 }
-            }
-            // Name is not Sulfaras
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                // Decrease sellIn
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            // SellIn more than 0
-            if (this.items[i].sellIn < 0) {
-                // Item not aged brie
-                if (this.items[i].name != 'Aged Brie') {
-                    //Imte not BSP
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        //Item quality not 0
-                        if (this.items[i].quality > 0) {
-                            // Item not sulfara
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                //Decrease quality
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    // Item is BSP
-                    } else {
-                        // Decrease quality
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                // Item is aged brie
-                } else {
-                    //Item quality more than 50
+                this.items[i].sellIn--
+            } else if (itemName === 'Backstage passes to a TAFKAL80ETC concert') {
+                this.items[i].sellIn--
+                if (this.items[i].quality < 50 && this.items[i].sellIn > 0) {
                     if (this.items[i].quality < 50) {
-                        // Increase quality
-                        this.items[i].quality = this.items[i].quality + 1
+                        this.items[i].quality++
+                        if (this.items[i].sellIn < 11) {
+                            this.items[i].quality++
+                        }
+                        if (this.items[i].sellIn < 6) {
+                            this.items[i].quality++
+                        }
+                    } else {
+                        this.items[i].quality = 0
+                    }
+                    if (this.items[i].quality > 50) {
+                        this.items[i].quality = 50
                     }
                 }
+            } else if (itemName === 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = 80
+            } else {
+                if (this.items[i].sellIn > 0) {
+                    this.items[i].quality--
+                    this.items[i].sellIn--
+                } else {
+                    this.items[i].quality--
+                    this.items[i].quality--
+                    this.items[i].sellIn--
+                }
+            }
+            if (this.items[i].quality < 0) {
+                this.items[i].quality = 0
             }
         }
 
